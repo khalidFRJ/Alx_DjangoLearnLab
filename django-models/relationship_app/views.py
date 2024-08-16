f18f6737a028b98f664748a29d42239679d6911e
KHALID ELFARAJ <162966628+khalidFRJ@users.noreply.github.com>
2024-08-16 21:59:07 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book
-
+from django.contrib.auth import login
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})

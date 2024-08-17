218489c7213d9b29cddb7b18bae9e52ecc236749
KHALID ELFARAJ <162966628+khalidFRJ@users.noreply.github.com>
2024-08-17 10:43:21 +0100

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -16,3 +16,12 @@ urlpatterns = [
     path('register/', SignUpView.as_view(), name='register'),
 ]
 
+from django.urls import path
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns = [
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
+]
+

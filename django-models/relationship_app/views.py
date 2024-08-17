e037240e3319232a305b039f22148b44ebede71b
KHALID ELFARAJ <162966628+khalidFRJ@users.noreply.github.com>
2024-08-17 11:45:33 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -19,6 +19,18 @@ def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+
 
 from .models import Book, Library
 UserCreationForm()", "relationship_app/register.html

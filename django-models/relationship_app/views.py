1acff87a1039534706961e1737a61439a63cdf7e
KHALID ELFARAJ <162966628+khalidFRJ@users.noreply.github.com>
2024-08-17 11:24:47 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -68,15 +68,7 @@ def member_view(request):
     return render(request, 'member_view.html')
 
 
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
 
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
-
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
 
 
 

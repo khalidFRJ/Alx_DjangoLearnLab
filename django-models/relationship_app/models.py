ec509abf7980913fc13a73f792f12f785b24a4d5
KHALID ELFARAJ <162966628+khalidFRJ@users.noreply.github.com>
2024-08-17 20:53:15 +0100

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -59,3 +59,25 @@ def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
 
 
+
+# models.py
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+
+

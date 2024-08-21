daf8cd9624fdb4b453a7e08dc69794685575ea28
KHALID ELFARAJ <162966628+khalidFRJ@users.noreply.github.com>
2024-08-21 11:45:11 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,12 +5,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import MyModel
 
 @permission_required('myapp.can_view', raise_exception=True)
-def mymodel_list(request):
+def book_list(request):
     objects = MyModel.objects.all()
     return render(request, 'myapp/mymodel_list.html', {'objects': objects})
 
 @permission_required('myapp.can_edit', raise_exception=True)
-def mymodel_edit(request, pk):
+def book(request, pk):
     obj = get_object_or_404(MyModel, pk=pk)
     # Handle the edit logic here
     return render(request, 'myapp/mymodel_edit.html', {'object': obj})

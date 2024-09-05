cca19cc6b8096f42273feb57aa14b22fa61a7c63
khalid <elfarajkhalid525@gmail.com>
2024-09-05 11:25:39 +0100

    commit

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('books/<int:pk>/', BookListView.as_view(), name='book/list'),                # List all books
     path('books/detail', BookDetailView.as_view(), name='book/detail'),   # Retrieve a single book by ID
     path('books/create', BookCreateView.as_view(), name='book/create'),        # Add a new book
-    path('books/Update', BookUpdateView.as_view(), name='book/update'), # Modify an existing book
+    path('books/update', BookUpdateView.as_view(), name='book/update'), # Modify an existing book
     path('books/delete', BookDeleteView.as_view(), name='book/delete'), # Remove a book
 ]

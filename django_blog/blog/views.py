f9d1c8b640c5a4e0ee9bfd754ddf61081ce7865a
khalid <elfarajkhalid525@gmail.com>
2024-09-11 21:26:16 +0100

    commit

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
+
+
+
+@login_required
+def profile_edit(request):
+    if request.method == 'POST':
+        user = request.user
+        user.email = request.POST.get('email')
+        user.save()
+        return redirect('profile')
+    return render(request, 'profile_edit.html')
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+    

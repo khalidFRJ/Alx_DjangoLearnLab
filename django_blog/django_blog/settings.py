b9012d9168cb47fd251b7e6b63bd09897ef26718
khalid <elfarajkhalid525@gmail.com>
2024-09-11 21:51:18 +0100

    commit

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -125,8 +125,9 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR /"django_blog/static"),
+   os.path.join(BASE_DIR /"blog/static"),
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

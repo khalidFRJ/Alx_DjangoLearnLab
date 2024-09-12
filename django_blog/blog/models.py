a4a875ea465f99868f42a264dea7f20d965614bc
khalid <elfarajkhalid525@gmail.com>
2024-09-12 19:02:51 +0100

    commit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,6 +7,7 @@ from taggit.managers import TaggableManager
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    published_date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # Add the TaggableManager to handle tags

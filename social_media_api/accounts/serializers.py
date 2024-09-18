b91133604078dba99bd35484003994f075ef9836
khalid <elfarajkhalid525@gmail.com>
2024-09-18 13:30:47 +0100

    commit

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password', 'bio', 'profile_picture', 'email']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None),
+        )
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ['key']
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'bio', 'profile_picture', 'followers']
+
